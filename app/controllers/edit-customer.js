4788cd410d49c120b3897f557f4c934848ed0278
Cassidy Foust <cassidy.foust@gmail.com>
2022-01-23 13:35:23 -0800

    new attribute inputs clear on save

--- app/controllers/edit-customer.js
+++ app/controllers/edit-customer.js
@@ -1,15 +1,19 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
+import { set } from '@ember/object';
+
 
 // a controller is a place to define 'actions', such as the things that should happen
 // when a user clicks a button to create/read/edit/delete a record
 
+
 export default class EditCustomerController extends Controller {
+
     @action
     addAttribute() {
       // get the input values from the .hbs template
-      let newAttributeName = this.newAttributeName;
-      let newAttributeValue = this.newAttributeValue;
+      let newAttributeName = this.attributeName;
+      let newAttributeValue = this.attributeValue;
       let newAttributes = this.model.attributes;
       newAttributes[newAttributeName] = newAttributeValue;
       this.store.findRecord('customer', this.model.id).then
@@ -18,7 +22,10 @@ export default class EditCustomerController extends Controller {
         customer.attributes = newAttributes;
         customer.save();
         });
-    }
+    this.set('attributeName', null);
+    this.set('attributeValue', null);
+    this.model.reload();
+  }
 
     @action
     deleteAttribute(attributeToDelete) {
@@ -30,6 +37,7 @@ export default class EditCustomerController extends Controller {
           customer.attributes = newAttributes;
           customer.save();
         });
+      this.model.reload();
     }
 
     @action
@@ -41,6 +49,7 @@ export default class EditCustomerController extends Controller {
           customer.attributes = newAttributes;
           customer.save();
         });
+      this.model.reload();
     }
 
     @action
--- app/routes/edit-customer.js
+++ app/routes/edit-customer.js
@@ -7,13 +7,7 @@ export default class EditCustomerRoute extends Route {
 
   async model(customerObject) {
     let customer;
-    this.store.findRecord('customer', customerObject.id);
-    await fetch(`http://localhost:1323/customers/${customerObject.id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        customer = data.customer;
-        customer.last_updated = new Date(customer.last_updated * 1000);
-      });
+    customer = this.store.findRecord('customer', customerObject.id);
     return customer;
   }
 }
